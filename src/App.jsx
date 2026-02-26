bbb292949e557aad11caa5af2a501c3a2f160873
IssaShergy <IssaShergy@users.noreply.github.com>
2026-02-26 19:44:50 +0300

    add orderpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import  {Routes, Route } from 'react-router'
+import { HomePage } from './Pages/HomePage'
 import './App.css'
+import { CheckoutPage } from './Pages/CheckoutPage'
+import { OrderPage } from './Pages/OrderPage'
+import { TrackingPage } from './Pages/trackingPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/Checkout" element={ <CheckoutPage/> } />
+      <Route path="/orders" element={ <OrderPage/> } />
+      <Route path="/tracking" element={ <TrackingPage/> } />
+    </Routes>
+   
     </>
   )
 }
